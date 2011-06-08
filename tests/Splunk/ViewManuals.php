51b5d07c084c22a8ca8bd2041e12d915df5b1c6e
Robert Van Dell <rvandell@splunk.com>
2011-06-08 20:44:40 +0000

    New tests.

---
+++ tests/Splunk/ViewManuals.php
@@ -0,0 +1,36 @@
+<?php
+
+class Splunk_ViewManuals extends AbstractAction
+{
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->_users = array
+        (
+            'admin'          => TRUE,
+            'anonymous'      => FALSE,
+            'logged_in'      => FALSE,
+            'splunk_preview' => FALSE,
+            'storm_preview'  => FALSE,
+            'employee'       => FALSE,
+            'splunk_docteam' => TRUE,
+            'storm_docteam'  => FALSE,
+            'docteam'        => TRUE
+        );
+    }
+    
+    protected function _allowed($user)
+    {
+        $this->open("/Documentation:Splunk:Manuals");
+        $this->assertEquals("Documentation:Splunk:Manuals - PonyDocs", $this->getTitle(), $user);
+        $this->assertTrue($this->isElementPresent("link=Edit"), $user);
+    }
+    
+    protected function _notAllowed($user)
+    {
+        $this->open("/Documentation:Splunk:Manuals");
+        $this->assertNotEquals("Documentation:Splunk:Manuals - PonyDocs", $this->getTitle(), $user);
+        $this->assertFalse($this->isElementPresent("link=Edit"), $user);
+    }
+}
