239db3b087395a31bd589beaecfd1377c08041eb
Heather Hunsinger <hhunsinger@splunk.com>
2011-06-08 00:45:33 +0000

    Add UserRightsManagment test WEB-5236

---
+++ tests/Any/UserRightsManagement.php
@@ -0,0 +1,51 @@
+<?php
+
+class Any_MoveTopic extends AbstractAction {
+	public function setUp() {
+
+		parent::setUp();
+
+		$this->_users = array
+		(
+    		'admin'          => TRUE,
+    		'anonymous'      => FALSE,
+    		'logged_in'      => FALSE,
+    		'splunk_preview' => FALSE,
+    		'storm_preview'  => FALSE,
+    		'employee'       => FALSE,
+    		'splunk_docteam' => FALSE,
+    		'storm_docteam'  => FALSE,
+    		'docteam'        => FALSE
+		);
+
+	}
+
+    protected function _allowed($user)
+    {
+		print "allowed: ".$user."\n";
+		$this->open("/Main_Page");
+		$this->click("link=Special pages");
+		$this->waitForPageToLoad("10000");
+		$this->click("link=User rights management");
+		$this->waitForPageToLoad("10000");
+		$this->type("username", "RandomUser");
+		$this->click("css=input[type=submit]");
+		$this->waitForPageToLoad("10000");
+		$this->click("wpGroup-employees");
+		$this->type("wpReason", "testing");
+		$this->click("saveusergroups");
+		$this->waitForPageToLoad("10000");
+		// User successfully added to group
+		$this->assertTrue($this->isTextPresent("Member of: employees"));
+    }
+
+    protected function _notAllowed($user)
+    {
+		print "not allowed: ".$user."\n";
+		$this->open("/Special:UserRights");
+		// Cannot access user rights management functionality
+		$this->assertTrue($this->isTextPresent("You do not have permission to do that"));
+    }
+}
+
+?>
