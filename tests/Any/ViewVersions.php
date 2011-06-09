0adeac6db71e777f5196790aeb9ad2f1c72ce05d
Robert Van Dell <rvandell@splunk.com>
2011-06-09 16:47:40 +0000

    Added new tests.

---
+++ tests/Any/ViewVersions.php
@@ -0,0 +1,52 @@
+<?php
+
+class Any_ViewVersions extends AbstractAction {
+	public function setUp() {
+
+		parent::setUp();
+
+		$this->_users = array
+		(
+    		'admin'          => TRUE,
+    		'anonymous'      => FALSE,
+    		'logged_in'      => FALSE,
+    		'splunk_preview' => FALSE,
+    		'storm_preview'  => FALSE,
+    		'employee'       => FALSE,
+    		'splunk_docteam' => FALSE,
+    		'storm_docteam'  => FALSE,
+    		'docteam'        => TRUE
+		);
+
+	}
+
+    protected function _allowed($user)
+    {
+		$this->open("/Documentation:Splunk:Versions");
+		// Content can be seen
+		$this->assertTrue($this->isTextPresent("Version 1.0 (released)"), $user);
+		$this->open("/index.php?title=Documentation:Splunk:Versions&action=edit");
+		// Content can be edited
+		$this->assertEquals("{{#version:1.0|released}}\n{{#version:2.0|preview}}\n{{#version:3.0|unreleased}}", $this->getValue("wpTextbox1"), $user);
+		$this->open("/index.php?title=Documentation%3ASplunk%3AVersions&action=historysubmit&diff=148&oldid=49");
+		$this->waitForPageToLoad("10000");
+		// Revision comparison can be seen
+		$this->assertTrue($this->isTextPresent("Version 1.0 (released)"), $user);
+    }
+
+    protected function _notAllowed($user)
+    {
+        $this->open("/Documentation:Splunk:Versions");
+		// Content cannot be seen
+		$this->assertFalse($this->isTextPresent("Version 1.0 (released)"), $user);
+		$this->open("/index.php?title=Documentation:Splunk:Versions&action=edit");
+		// Content cannot be seen
+		$this->assertNotEquals("{{#version:1.0|released}}\n{{#version:2.0|preview}}\n{{#version:3.0|unreleased}}", $this->getValue("wpTextbox1"), $user);
+		$this->open("/index.php?title=Documentation%3ASplunk%3AVersions&action=historysubmit&diff=148&oldid=49");
+		$this->waitForPageToLoad("10000");
+		// Revision comparison cannot be seen
+		$this->assertFalse($this->isTextPresent("Version 1.0 (released)"), $user);
+    }
+}
+
+?>
---
+++ tests/Splunk/EditTopic.php
@@ -0,0 +1,38 @@
+<?php
+
+class Splunk_EditTopic extends AbstractAction
+{
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->_users = array
+        (
+            'admin'          => TRUE,
+            'anonymous'      => FALSE,
+            'logged_in'      => FALSE,
+            'splunk_preview' => FALSE,
+            'storm_preview'  => FALSE,
+            'employee'       => TRUE,
+            'splunk_docteam' => TRUE,
+            'storm_docteam'  => FALSE,
+            'docteam'        => FALSE
+        );
+    }
+    
+    protected function _allowed($user)
+    {
+        $this->select('docsManualSelect', 'label=Splunk User Manual');
+        $this->waitForPageToLoad('10000');
+        $this->assertEquals('Documentation:Splunk:User:SplunkOverview:1.0 - PonyDocs', $this->getTitle(), $user);
+        $this->assertTrue($this->isElementPresent('link=Edit'), $user);
+    }
+    
+    protected function _notAllowed($user)
+    {
+        $this->select('docsManualSelect', 'label=Splunk User Manual');
+        $this->waitForPageToLoad('10000');
+        $this->assertEquals('Documentation:Splunk:User:SplunkOverview:1.0 - PonyDocs', $this->getTitle(), $user);
+        $this->assertFalse($this->isElementPresent('link=Edit'), $user);
+    }
+}
